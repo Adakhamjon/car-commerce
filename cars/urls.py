84c5db13f09ec128f0890f3cb3dfa9031f0da731
unknown <mukhammadjonovich98@gmail.com>
2023-11-30 14:38:49 +0500

    completed search functionalities

--- cars/urls.py
+++ cars/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-path("",cars, name = 'cars')
+path("",cars, name = 'cars'),
+path("<int:id>",car_detail,name='car_detail'),
+path("search",search,name='search')
 ]
